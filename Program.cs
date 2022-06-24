a56a0b339224e04a3441062863bef54b1c1de962
large-legume <38702175+large-legume@users.noreply.github.com>
2022-06-24 16:25:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Configuration;
+using System.IO.Compression;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //stringify settings
+            string srce = ConfigurationManager.AppSettings.Get("srce");
+            string dest = ConfigurationManager.AppSettings.Get("dest");
+            int mnth = int.Parse(ConfigurationManager.AppSettings.Get("mnth"));
+            string type = ConfigurationManager.AppSettings.Get("type");
+
+            //specify source
+            DirectoryInfo DirInfo = new DirectoryInfo(@srce);
+
+            //specify desired month
+            DateTime zipmnth = DateTime.UtcNow.AddMonths(-mnth);
+            DateTime zipmnth2 = new DateTime(zipmnth.Year, zipmnth.Month, 1);
+            DateTime zipmnth3 = new DateTime(zipmnth.Year, zipmnth.Month, DateTime.DaysInMonth(zipmnth.Year,zipmnth.Month));
+
+            //create folder naming schema
+            string schema = zipmnth.Year + "_" + zipmnth.Month + "_" + type;
+            //create folder to be zipped
+            string getzipped = Path.Combine(dest, schema);
+            Directory.CreateDirectory(getzipped);
+
+            //find files 
+            var files = from f in DirInfo.EnumerateFiles()
+                        where f.CreationTimeUtc >= zipmnth2
+                        where f.CreationTimeUtc <= zipmnth3
+                        select f;
+
+            //move files, create folder
+            foreach (var f in files)
+            {
+                string filename = Path.Combine(getzipped, f.Name);
+                //list files
+                Console.WriteLine("{0}", f.Name);
+                File.Move(f.FullName, filename);
+            }
+            //zip the folder (and handle possible duplicate exception)
+            string zipdest = Path.Combine(dest, schema + ".zip");
+            ZipFile.CreateFromDirectory(getzipped, zipdest);
+            Directory.Delete(getzipped, true);
+
+        }
+    }
+}
